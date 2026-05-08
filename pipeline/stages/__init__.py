b96d1fc096bd4fed1400dc929738a4924a875519
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2026-05-08 17:56:44 -0500

    fix: corregir bugs en setup.py y stages/__init__.py para Colab install

--- pipeline/stages/__init__.py
+++ pipeline/stages/__init__.py
@@ -2,5 +2,6 @@
 
 from pipeline.stages.template_extractor import TemplateExtractor, PLACEHOLDER_RULES
 from pipeline.stages.message_splitter import MessageSplitter, SplitResult
+from pipeline.stages.rule_classifier import RuleClassifier
 
-__all__ = ["TemplateExtractor", "PLACEHOLDER_RULES", "MessageSplitter", "SplitResult"]
+__all__ = ["TemplateExtractor", "PLACEHOLDER_RULES", "MessageSplitter", "SplitResult", "RuleClassifier"]
